0d16bc8ed7b57757547be7ffa0af7622f6c2dc36
Terrych <vssst@live.com>
2015-02-03 16:26:41 -0500

    Enemies are attacking

---
+++ Blight Crusaders/Assets/Scripts/EnemyAttack.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyAttack : MonoBehaviour {
+
+	double colddown = 0.0;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+				colddown += Time.deltaTime;
+				//bool cd = GameObject.GetComponent<CharacterState>().on_cooldown_huh;
+
+				if (colddown > 3.0 && colddown < 5.0) {
+						gameObject.renderer.material.color = Color.red;
+				} else if (colddown > 5.0) {
+
+						//attack here
+
+						colddown = 0.0;
+			
+						gameObject.renderer.material.color = Color.white;
+				}
+		}
+}
