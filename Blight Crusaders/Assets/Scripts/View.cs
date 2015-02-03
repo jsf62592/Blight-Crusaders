881dfb02832d650942ff6fb180e6e62151d87310
Terrych <vssst@live.com>
2015-02-03 16:17:11 -0500

    #3

--- Blight Crusaders/Assets/Scripts/View.cs
+++ Blight Crusaders/Assets/Scripts/View.cs
@@ -2,14 +2,37 @@
 using System.Collections;
 
 public class View : MonoBehaviour {
-
+	
 	// Use this for initialization
 	void Start () {
-	
+		
 	}
 	
-	// Update is called once per frame
-	void Update () {
+	RuntimePlatform platform = Application.platform;
 	
+	void Update(){
+		if (platform == RuntimePlatform.Android || platform == RuntimePlatform.IPhonePlayer){
+			if (Input.touchCount > 0 && Input.GetTouch (0).phase == TouchPhase.Began) {
+				Debug.Log ("android running");
+				Ray ray = Camera.main.ScreenPointToRay (Input.GetTouch (0).position);
+				RaycastHit hit;
+				
+				if (Physics.Raycast (ray, out hit)) {
+					hit.collider.GetComponent<Touch>().TouchResponse ();  
+				}
+			}
+		}else 
+		if(platform == RuntimePlatform.WindowsEditor | platform == RuntimePlatform.OSXEditor){
+			if (Input.GetMouseButtonDown(0)) {
+				Debug.Log("editor running");
+				Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+				RaycastHit hit;
+				
+				if (Physics.Raycast (ray, out hit)) {
+					hit.collider.GetComponent<Touch>().TouchResponse ();  
+				}
+			}
+		}
+		
 	}
 }
