626cb7f39a7dce6809fdecec36c384f9c170d9fa
Terrych <vssst@live.com>
2015-02-03 16:10:29 -0500

    SetupForMac

---
+++ Blight Crusaders/Assets/Scripts/Touch.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Touch : MonoBehaviour {
+
+	double cooldown = 0.0; //temporary cooldown variable for initial touch story color change
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	void Update(){
+		if (cooldown > 0.0){ 
+			Debug.Log(name + "cooldown: " + cooldown); 
+			cooldown -= Time.deltaTime;
+		}else{
+			gameObject.renderer.material.color = Color.white;
+		}
+	}
+
+	public void TouchResponse(){
+		Debug.Log(name);
+		if (tag == "Player" && cooldown <= 0.0) {
+			
+			Debug.Log ("touch.cs touched");
+		//if(true){
+			gameObject.renderer.material.color = Color.blue;
+			cooldown = 3.0;
+		}
+	}
+}
