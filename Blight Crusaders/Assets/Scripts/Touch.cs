a01c269af0a96299c353833c29c0e7c12ed5320e
jpmeenagh <jpmeenagh.capstone@gmail.com>
2015-02-02 19:14:54 -0500

    Milestone 1 touch
    - Touch.cs - script that must be put on all capsules to recognize
    touches. They must be tagged as either "Enemy" or "Player". If a player
    is touched they turn blue for 3 seconds.
    - View.cs - I assume this goes on the camera. I added code to check for
    touch input and call the objects' touch scripts. The code will actually
    check if the code is being run in the editor and register clicks and
    touches instead
    -BattleRush2.apk - new build with all the scripts attached and working

---
+++ Blight Crusaders/Assets/Scripts/Touch.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Touch : MonoBehaviour {
+
+	double cooldown = 0.0; //temporary cooldown variable for initial touch story color change
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	void Update(){
+		if (cooldown > 0){ 
+			Debug.Log(name + "cooldown: " + cooldown); 
+			cooldown -= Time.deltaTime;
+		}else{
+			gameObject.renderer.material.color = Color.white;
+		}
+	}
+
+	public void TouchResponse(){
+		Debug.Log(name);
+		if (tag == "Player" && cooldown <= 0) {
+			gameObject.renderer.material.color = Color.blue;
+			cooldown = 3.0;
+		}
+	}
+}
