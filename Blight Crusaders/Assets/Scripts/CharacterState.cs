263927e4febdf069aa02ff5acbde39dc910096bd
obrien1jj <obrien1jj@gmail.com>
2015-02-12 05:36:06 -0500

    made movement stuff when stuff gets hurt.
    omg i'm tired q-q

--- Blight Crusaders/Assets/Scripts/CharacterState.cs
+++ Blight Crusaders/Assets/Scripts/CharacterState.cs
@@ -2,16 +2,49 @@
 using System.Collections;
 
 public class CharacterState : MonoBehaviour {
-	private float time_next_tick = 0;
-	private int cooldown = 20;
-	private int max_cooldown = 20;
 
-	private int health;
-	public int health_max = 100;
+	//the total length of the screen.  It gets this from outside this scrip.
+	private float screen_length;
+
+	private float time_next_tick;
+	private int cooldown;
+
+	public int health_max;
+	public int health_current;
+	public float health_percent;
+
+	//how far it should be from the edge of the screen
+	public float distance;
+	//initial offset from the center of the screen.  cannot be 0, as this is used to determine which way this should move when damaged
+	public float distance_initial_offset;
 
 	// Use this for initialization
 	void Start () {
-		this.health = this.health_max;
+		this.time_next_tick = 0;
+		this.cooldown = 0;
+
+		this.health_current = this.health_max;
+		this.health_percent = (float)this.health_current / (float)this.health_max;
+
+
+		this.screen_length = 20; print ("!!!ALERT!!!:  CharacterState.cs USING DUMMY   (/0 ^0)/ ^ I_____I");
+
+		this.distance = ((this.screen_length / 2) - Mathf.Abs(distance_initial_offset)) * this.health_percent;
+
+
+		//if the initial offset is wrong, complain and blow up
+		if (distance_initial_offset == 0){
+			throw new UnityException("CharacterState.cs exception:  distance_initial_offset is set to 0 on entity: " + this.name);
+		}
+		if (Mathf.Abs(distance_initial_offset) >= (this.screen_length / 2)){
+			throw new UnityException("CharacterState.cs exception:  distance_initial_offset >= (screen_length / 2)");
+		}
+
+		move_according_to_health ();
+
+
+
+		print ("CharacterState Debug button enabled");
 	}
 	
 	// Update is called once per frame
@@ -23,10 +56,15 @@ public class CharacterState : MonoBehaviour {
 			}
 			//print ("Cooldown:  UPDATE  |  time left:  " + this.cooldown + " | on_cd?: " + this.on_cooldown_huh());
 		}
+
+		if(Input.GetKeyDown("f")){
+			print ("CharacterState Debug button pressed");
+			this.take_damage(10);
+		}
 	}
 
-	public void cooldown_start(){
-		this.cooldown = this.max_cooldown;
+	public void cooldown_start(int given_cooldown){
+		this.cooldown = given_cooldown;
 	}
 
 	public bool on_cooldown_huh(){
@@ -34,6 +72,30 @@ public class CharacterState : MonoBehaviour {
 	}
 
 	public void take_damage(int given_damage){
-
+		this.health_current = this.health_current - given_damage;
+		if(health_current <= 0){
+			death();
 		}
+		this.health_percent = (float)this.health_current / (float)this.health_max;
+		this.distance = ((this.screen_length / 2) - Mathf.Abs(distance_initial_offset)) * this.health_percent;
+		move_according_to_health();
+	}
+
+	private void move_according_to_health(){
+		float new_x_position;
+		//if it's on the left side
+		if(distance_initial_offset < 0){
+			new_x_position = distance;
+		}
+		//if it's on the right side
+		else{
+			new_x_position = screen_length - distance;
+		}
+		print (new_x_position);
+		this.transform.position = new Vector3 (new_x_position, this.transform.position.y, this.transform.position.z);
+	}
+
+	private void death(){
+		Destroy(this.gameObject);
+	}
 }
