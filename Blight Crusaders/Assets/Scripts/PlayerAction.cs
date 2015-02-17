0d9de70976334bad3accdfe95c7b772927bb13a6
jpmeenagh <jpmeenagh.capstone@gmail.com>
2015-02-16 20:04:18 -0500

    Added Drag basic attack
    Replaced Touch with PlayerAction and View with Interface
    
    To set up
    -Put Interface on camera and Player Action on the two PCs
    -Tag enemy as Enemy and player as Player
    -put CharacterState on PCs
    
    Change:
    When enemy player cooldown is at 0, you can tap and drag from a PC to an
    enemy to attack.

---
+++ Blight Crusaders/Assets/Scripts/PlayerAction.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerAction : MonoBehaviour {
+	public int changeColors = 0;
+	GameObject target;
+
+	// Use this for initialization
+	void Start () {
+		target = null;
+	}
+
+	void Update(){
+		if (changeColors > 0) { changeColors--; }
+		if (changeColors == 0 && target != null) { 
+			target.renderer.material.color = Color.white;
+			target = null;
+		}
+	}
+
+	public void Attack(GameObject targeted){
+		target = targeted;
+		Debug.Log (name + " Attacks " + target.name);
+		ChangeColors();
+		GetComponent<CharacterState> ().cooldown_start();
+	}
+
+	void ChangeColors(){
+		target.renderer.material.color = Color.red;
+
+		changeColors = 10;
+	}
+
+	public void Select(){
+		gameObject.renderer.material.color = Color.blue;
+	}
+
+	public void DeSelect(){
+		gameObject.renderer.material.color = Color.white;
+	}
+
+}
