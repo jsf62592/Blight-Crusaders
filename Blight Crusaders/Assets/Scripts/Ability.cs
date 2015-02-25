b95d7e8cd2f5e9dbf38c380164863dec28e8da46
obrien1jj <obrien1jj@gmail.com>
2015-02-25 05:54:24 -0500

    Did some ability stuff.  More to come tomorrow
    Made an abstract parent ability.  needs more work.
    Made a dummy message for the queue.  meh.
    Going to make 2 actual abilities tomorrow.
    Might be making a class for status effects.  We'll see.

---
+++ Blight Crusaders/Assets/Scripts/Ability.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class Ability : MonoBehaviour {
+
+	//status effects that will be inflicted upon this ability's target
+	public ArrayList status_effects;
+
+	//this is the cooldown on the ability
+	public int max_cooldown = 1;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {}
+
+	//does what this ability does.  should only be called by a message on the ability queue
+	//it's late though and i currently cannot be bothered to figure out a good way to restrict access to this
+	//so expect this to be changed
+	public static void message_callback(){
+
+
+	}
+
+	//add a message onto the ability queue
+	public void add_to_queue(){
+		Ability_Message message = new Ability_Message();
+	}
+}
