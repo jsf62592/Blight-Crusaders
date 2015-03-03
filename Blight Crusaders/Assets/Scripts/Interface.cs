2ccb87815fe99a59cfab0c19fb5266e151694c29
jpmeenagh <jpmeenagh.capstone@gmail.com>
2015-03-03 15:06:09 -0500

    put in basic structure of gesture input for mouse

--- Blight Crusaders/Assets/Scripts/Interface.cs
+++ Blight Crusaders/Assets/Scripts/Interface.cs
@@ -12,11 +12,28 @@ public class Interface : MonoBehaviour {
 	public GameObject selected;
 	public GameObject targeted;
 	CharacterState state;
+
+
+	public Vector2 targetScreenPosition; 
+	public Vector2 target1; //top right
+	public Vector2 target2; //bottom right
+	public Vector2 target3; //bottom left
+	public Vector2 target4; //top left
+	public float gestureTargetXOffset = 20; //distances to place the target to record gesture
+	public float gestureTargetYOffset = 20;
+	public float gestureTargetRadius = 10;
+	public int[] LastFourTargetHits; //which target was hit 1-4
+
+	public Boolean draw;
+	public Texture targetImage;
+
+	public Vector2 mousePos;
 	
 	// Use this for initialization
 	void Start () {
 		selected = null;
 		targeted = null;
+		targetImage = Resources.Load("target") as Texture;
 		
 	}
 	
@@ -44,8 +61,7 @@ public class Interface : MonoBehaviour {
 			//if touch on a player and is released over another character, attack
 			if (Input.touchCount > 0 && Input.GetTouch (0).phase == TouchPhase.Ended && state.getActive()) {
 				RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint (Input.mousePosition), Vector2.zero);
-
-				
+							
 				//If an object is touched? 
 				if (hit != null) {
 					targeted = hit.collider.gameObject;
@@ -81,31 +97,72 @@ public class Interface : MonoBehaviour {
 					}
 				}
 			}
-			//if mouse was clicked on a player and is released over another character, attack
-			if (Input.GetMouseButtonUp (0) && selected!= null) {
-					RaycastHit2D hit2 = Physics2D.Raycast(Camera.main.ScreenToWorldPoint (new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10)), Vector2.zero);
-				
-				if (hit2.collider != null) {
+			//if attack input is is progress (a player was clicked)
+			if (selected!= null && targeted == null) {
+			    RaycastHit2D hit2 = Physics2D.Raycast(Camera.main.ScreenToWorldPoint (new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10)), Vector2.zero);
+
+				if (hit2.collider != null && hit2.collider.gameObject != selected) {
 					targeted = hit2.collider.gameObject;
+					setTargets(); //set the values for the targets based on the screen position
 					state = hit2.collider.GetComponent<CharacterState>();
-					if(targeted != selected){
-						Debug.Log("HGERE");
-						Fireball f = new Fireball();
-						Message m = new Message(selected, targeted, f);
-						GameManager.instance.AddPlayerAction(m);
-             		}
-					
-					selected.GetComponent<PlayerAction>().DeSelect();
-					ResetInput();
-					
-				}else{
-					selected.GetComponent<PlayerAction>().DeSelect();
-					ResetInput();
 				}
+			}
+
+			if(targeted != null){ //if we have a target
+				targetScreenPosition = Camera.main.WorldToScreenPoint(targeted.transform.position);//the onscreen position of the target
+				recordTargetHits(Input.mousePosition.x, Input.mousePosition.y); //record the last targets hit 
+				checkTargetHits(); //check if they form an X or O
+			}
+	
+			//reset input if mouse is up
+			if(Input.GetMouseButtonUp(0)){
+				selected.GetComponent<PlayerAction>().DeSelect ();
+				ResetInput();
+			}
+			
+
+		}
+	}
+
+	void OnGUI() {
+		if (draw) {
+			if (!targetImage) {
+					Debug.LogError ("Assign a Texture in the inspector.");
+					return;
+			}
+			if(targeted != null){
+				GUI.DrawTexture (new Rect (target1.x - gestureTargetRadius, 
+			                           target1.y - gestureTargetRadius, 
+			                           target1.x + gestureTargetRadius,
+			                           target1.y - gestureTargetRadius), targetImage, ScaleMode.ScaleToFit, true, 10.0F);
+		
 			}
 		}
 	}
+
+
+	public void setTargets(){
+		target1 = new Vector2 (targetScreenPosition.x + gestureTargetXOffset, targetScreenPosition.y + gestureTargetYOffset);
+		target2 = new Vector2 (targetScreenPosition.x + gestureTargetXOffset, targetScreenPosition.y - gestureTargetYOffset);
+		target3 = new Vector2 (targetScreenPosition.x - gestureTargetXOffset, targetScreenPosition.y - gestureTargetYOffset);
+		target4 = new Vector2 (targetScreenPosition.x - gestureTargetXOffset, targetScreenPosition.y + gestureTargetYOffset);
+	}
+
+
+	//if the target
+	public void recordTargetHits(float X, float Y){
+		mousePos = new Vector2(X, Y);
 	
+		if (Math.Abs(Vector2.Distance(target1,mousePos)) <= gestureTargetRadius) { print ("TARGET1"); }
+		if (Math.Abs(Vector2.Distance(target2,mousePos)) <= gestureTargetRadius) { print ("TARGET2"); }
+		if (Math.Abs(Vector2.Distance(target3,mousePos)) <= gestureTargetRadius) { print ("TARGET3"); }
+		if (Math.Abs(Vector2.Distance(target4,mousePos)) <= gestureTargetRadius) { print ("TARGET4"); }
+	}
+
+
+	public void checkTargetHits(){
+		
+	}
 	
 	
 	public void ResetInput(){
