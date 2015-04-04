78667e385b3cacdb02ae2a4cfa4523ea78f91af4
obrien1jj <obrien1jj@gmail.com>
2015-04-04 16:32:39 -0400

    started an example

---
+++ Blight Crusaders/Assets/Scripts/SE_Plague_Bolt.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SE_Plague_Bolt : Status_Effect {
+
+	void Start(){
+		setup (10);
+	}
+	protected override void persistant_effect(){
+		state.take_damage (10);
+	}
+}
