bdcfe90a90dd6c66c10f87a56ce046610558d00f
obrien1jj <obrien1jj@gmail.com>
2015-04-04 16:35:30 -0400

    last thing didn't save everything

--- Blight Crusaders/Assets/Scripts/Ability_Plague_Bolt.cs
+++ Blight Crusaders/Assets/Scripts/Ability_Plague_Bolt.cs
@@ -2,14 +2,11 @@
 using System.Collections;
 
 public class Ability_Plague_Bolt : Ability {
+	void Start(){
+		setup (5, false, "Prefabs/Alch_Bomb");
+	}
+	protected override void attachEffects(GameObject given_target){
+		given_target.AddComponent<SE_Alch_Bomb> ();
+	}
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
 }
